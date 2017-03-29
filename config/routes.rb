d19e752397f7b1c23e6652219f14198077359de6
Patricia Wiesker <patricia@wiesker.de>
2017-03-29 17:29:23 +0800

    tying to fix

--- config/routes.rb
+++ config/routes.rb
@@ -14,11 +14,10 @@ Rails.application.routes.draw do
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get('/dashboard_host', {to: 'pages#dashboard_host'})
   get '/dashboard_guest', to: 'pages#dashboard_guest'
-
   resources :hobbies, only:[:index, :show]
+
 end
 
 
