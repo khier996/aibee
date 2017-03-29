079420dfa8025f92237412d0e105f4d46bad1493
Maksim Khier <maksimkhier@outlook.com>
2017-03-29 18:37:52 +0800

    Stuff

--- db/migrate/20170329065954_add_status_to_bookings.rb
+++ db/migrate/20170329065954_add_status_to_bookings.rb
@@ -1,5 +1,5 @@
 class AddStatusToBookings < ActiveRecord::Migration[5.0]
   def change
-    add_column :bookings, :status, :string, null: false
+    add_column :bookings, :status, :string
   end
 end
