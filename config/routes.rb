ec9bed6c670e91f6d9e2d811d2462e5791836a1c
Thomas Henderson <tbhenderson55@yahoo.com>
2017-03-29 16:38:34 +0800

    dashboard events now showing

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,12 @@ Rails.application.routes.draw do
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
 
+
   resources :hobbies do
     resources :events, only: [:new, :create, :edit, :update, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get('/dashboard_host', {to: 'pages#dashboard_host'})
 
 
 end
