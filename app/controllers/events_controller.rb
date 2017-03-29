f96ba05adb99b5d79ea991c962accfe9670c2333
Thomas Henderson <tbhenderson55@yahoo.com>
2017-03-29 11:40:08 +0800

    events edit update working

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -31,17 +31,32 @@ class EventsController < ApplicationController
   end
 
   def edit
+    @event = Event.find(params[:id])
+    @hobby = Hobby.find(params[:hobby_id])
     authorize @event
+
 
   end
 
   def update
+    @hobby = Hobby.find(params[:hobby_id])
+
+    @event = Event.find(params[:id])
+    @event.update(event_params)
     authorize @event
+
+    redirect_to hobby_path(@hobby)
 
   end
 
   def destroy
     authorize @event
+    event = Event.find(params[:id])
+    event.destroy
+
+    redirect_to hobby_path(@hobby)
+
+    # men_path(Man.all)
 
   end
 
