1a75659362ae70ee343863c388c9c33154f78793
Antoine Deconinck <adeconinck.sina@gmail.com>
2017-03-29 19:28:58 +0800

    update map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,6 @@
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
+
+//= require underscore
+//= require gmaps/google
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328082519) do
+ActiveRecord::Schema.define(version: 20170329095034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20170328082519) do
     t.integer  "review_count"
     t.boolean  "deleted"
     t.boolean  "hidden"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_hobbies_on_user_id", using: :btree
   end
 
