252a8f6589e91d1825f516bd60c235ccf5cd458a
Patricia Wiesker <patricia@wiesker.de>
2017-03-30 14:54:10 +0800

    Booking page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,8 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Event.all
     @events = policy_scope(Event).order(created_at: :desc)
-    # @events = Event.all
     @hobby = Hobby.find(params[:hobby_id])
     authorize @events
   end
@@ -15,34 +15,27 @@ class EventsController < ApplicationController
 
   def create
     @hobby = Hobby.find(params[:hobby_id])
-
     @event = Event.new(event_params)
     @event.hobby_id = @hobby[:id]
     authorize @event
-    # raise
     if @event.save
       redirect_to hobby_path(@hobby)
     else
       render :new
     end
-
   end
 
   def edit
     @event = Event.find(params[:id])
     @hobby = Hobby.find(params[:hobby_id])
     authorize @event
-
-
   end
 
   def update
     @hobby = Hobby.find(params[:hobby_id])
-
     @event = Event.find(params[:id])
     @event.update(event_params)
     authorize @event
-
     redirect_to hobby_path(@hobby)
   end
 
@@ -51,13 +44,7 @@ class EventsController < ApplicationController
     @event.destroy
     @hobby = Hobby.find(params[:hobby_id])
     authorize @event
-
     redirect_to dashboard_host_path
-
-    # redirect_to hobby_path(@hobby)
-
-    # men_path(Man.all)
-
   end
 
   private
