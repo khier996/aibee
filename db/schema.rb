24a40165fb9dafb6204fbf7e29ff6636f5817257
Antoine Deconinck <adeconinck.sina@gmail.com>
2017-03-30 12:14:18 +0800

    lat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329065954) do
+ActiveRecord::Schema.define(version: 20170329095034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,8 @@ ActiveRecord::Schema.define(version: 20170329065954) do
     t.integer  "review_count"
     t.boolean  "deleted"
     t.boolean  "hidden"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_hobbies_on_user_id", using: :btree
   end
 
