b437aa1063cbc87f77c536ae12cdc6261bdfb161
Maksim Khier <maksimkhier@outlook.com>
2017-03-30 16:29:05 +0800

    Some small fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 20170330061155) do
-
+ActiveRecord::Schema.define(version: 20170330063403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +38,7 @@ ActiveRecord::Schema.define(version: 20170330061155) do
     t.integer  "pax"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "status",     null: false
+    t.string   "status"
     t.index ["event_id"], name: "index_bookings_on_event_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
   end
@@ -76,10 +73,9 @@ ActiveRecord::Schema.define(version: 20170330061155) do
     t.integer  "review_count"
     t.boolean  "deleted"
     t.boolean  "hidden"
-    t.float    "average_score"
     t.float    "latitude"
     t.float    "longitude"
-
+    t.float    "average_score"
     t.index ["user_id"], name: "index_hobbies_on_user_id", using: :btree
   end
 
