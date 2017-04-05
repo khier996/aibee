3466b83c4373427de314d3864bf335c3338011c2
Patricia Wiesker <patricia@wiesker.de>
2017-04-05 19:15:26 +0800

    coursel

--- app/assets/javascripts/owl.carousel.js
+++ app/assets/javascripts/owl.carousel.js
@@ -17,15 +17,12 @@
  */
 
 $(document).ready(function(){
-
   $('.owl-carousel').owlCarousel({
-      stagePadding: 50,
-      lazyLoad: true,
-      center: true,
-      items:2,
       loop:true,
       margin:10,
-      nav:true,
+      autoplay: true,
+      autoplayTimeout: 2000,
+      autoplayHoverPause: true,
       responsive:{
           0:{
               items:1
