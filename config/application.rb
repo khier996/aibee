560c2631b0bafb5110f987fb02d62f9604f7a20f
Patricia Wiesker <patricia@wiesker.de>
2017-04-06 16:21:28 +0800

    fonts fixed

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,8 @@ module Aibee
     end
 
     config.action_view.embed_authenticity_token_in_remote_forms = true
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.enabled = true
+      config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
